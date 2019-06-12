7901336cc739342baef0570b23072a6cb5debe55
marcobellaccini <24960638+marcobellaccini@users.noreply.github.com>
2019-06-12 21:14:05 +0200

    Read header lines

--- pyagetool/pyagetool.py
+++ pyagetool/pyagetool.py
@@ -3,15 +3,30 @@ import re
 
 """Main module."""
 
+RL_MAX_CHARS = 200
+"""int: max number of chars per-line.
+"""
+
 class Age:
     """Main age format class"""
 
-    def readheader(self, path):
+    def parse_header(self, path):
+        """This method parses encrypted file header.
+
+        Args:
+            path: encrypted file path.
+
+        Returns:
+            The tuple (age_version, recipients).
+
+            If something goes wrong, it will raise an exception.
+        """
+
         with open(path) as f:
-            first_line = f.readline()
+            lines = [f.readline(RL_MAX_CHARS)]
             # parse file format version
             m = re.match(r"This is a file encrypted with age-tool.com, "
-                          "version (?P<age_version>[a-zA-Z0-9_.]+)", first_line)
+                          "version (?P<age_version>[a-zA-Z0-9_.]+)", lines[0])
             # check whether file is in age format
             if not m:
                 raise ValueError('Target file is not an age-tool file.')
@@ -19,6 +34,19 @@ class Age:
             age_version = m.group('age_version')
             if age_version != '1':
                 raise ValueError('Unsupported age-tool format version.')
+            # read the remaining part of the header and get the HMAC
+            b64_header_hmac = ""
+            while True:
+                line = f.readline(RL_MAX_CHARS)
+                if re.match(r"--- (?P<b64_header_hmac>[a-zA-Z0-9+/=]+)",
+                            line):
+                    break
+                if not line:
+                    raise ValueError('Missing header HMAC.')
+                lines.append(line)
 
+            print(b64_header_hmac)
+            for a in lines:
+                print(a)
 
-        return 'hello world'
+        return (age_version, 0)
--- tests/test_pyagetool.py
+++ tests/test_pyagetool.py
@@ -18,23 +18,37 @@ class TestPyagetool(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_readheader_notage(self):
-        """Test readheader method: not an age file"""
+    def test_parse_header_notage(self):
+        """Test parse_header method: not an age file"""
         age = pyagetool.Age()
         self.assertRaisesRegex(ValueError, ('Target file is not an '
                                             'age-tool file\.'),
-                               age.readheader, 'tests/fixtures/notage.age')
+                               age.parse_header, 'tests/fixtures/notage.age')
 
-    def test_readheader_unsuppo_form_ver(self):
-        """Test readheader method: unsupported format version"""
+    def test_parse_header_unsuppo_form_ver(self):
+        """Test parse_header method: unsupported format version"""
         age = pyagetool.Age()
         self.assertRaisesRegex(ValueError, ('Unsupported age-tool '
                                             'format version.'),
-                               age.readheader,
-                               'tests/fixtures/unsupp_form_ver.age')
+                               age.parse_header,
+                               'tests/fixtures/unsuppformver.age')
 
-    # def test_readheader(self):
-    #     """Test readheader method."""
+    def test_parse_header_no_header_hmac(self):
+        """Test parse_header method: missing header HMAC"""
+        age = pyagetool.Age()
+        self.assertRaisesRegex(ValueError, ('Missing header HMAC.'),
+                               age.parse_header,
+                               'tests/fixtures/noheadhmac.age')
+
+    def test_parse_header_bad_header_hmac(self):
+        """Test parse_header method: bad header HMAC"""
+        age = pyagetool.Age()
+        self.assertRaisesRegex(ValueError, ('Bad header HMAC.'),
+                               age.parse_header,
+                               'tests/fixtures/badheadhmac.age')
+
+    # def test_parse_header(self):
+    #     """Test parse_header method."""
     #     exp_header = """This is a file encrypted with age-tool.com, version 1
     #                     -> X25519 CJM36AHmTbdHSuOQL-NESqyVQE75f2e610iRdLPEN20
     #                     C3ZAeY64NXS4QFrksLm3EGz-uPRyI0eQsWw7LWbbYig
@@ -54,4 +68,4 @@ class TestPyagetool(unittest.TestCase):
     #                     --- fgMiVLJHMlg9fW7CVG+hPS5EAU4Zeg19LyCP7SoH5nA"""
     #
     #     age = pyagetool.Age()
-    #     self.assertEqual(age.readheader('fixtures/test.age'), exp_header)
+    #     self.assertEqual(age.parse_header('fixtures/test.age'), exp_header)
